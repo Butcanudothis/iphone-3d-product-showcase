c529c69d23fea32c24e4d1a93e62b5c96d104d95
Butcanudothis <akshayvarma.varma@gmail.com>
2023-05-03 01:31:31 +1000

    added basic layout with scroll functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-
+import Nav from './components/Nav.jsx';
+import Jumbotron from './components/Jumbotron.jsx';
+import SoundSection from './components/SoundSection.jsx';
+import DisplaySection from './components/DisplaySection.jsx';
 function App() {
 
   return (
     <div className="App">
-      Hello
+      <Nav/>
+      <Jumbotron/>
+      <SoundSection/>
+      <DisplaySection/>
+
     </div>
   );
 }
---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function DisplaySection() {
+  return (
+    <div className="display-section wrapper">
+      <h2 classname="title">New</h2>
+      <p className="text">Brilliant</p>
+      <span className="description">
+        A display that's upto 2x brighter in the sun
+      </span>
+      <button className="button"> Try Me!</button>
+      <button className="back-button">Top</button>
+    </div>
+  );
+}
+export default DisplaySection;
